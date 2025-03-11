7bf6566057b41cf5fd603f91815c21d375fb8a34
ihtisham-sudo <ahsaanullah174@gmail.com>
2025-03-12 01:02:03 +0500

    first commit

---
+++ mvc-nubisha-rana/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace mvc_nubisha_rana.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
